0cfce1b161e1b555e9f3c739fcfe1b4b72fbc0c5
orvyl <OFTumaneng@smartemoneyinc.com>
2015-10-02 15:58:21 +0800

    initial workflow-api module

---
+++ api/src/main/java/com/orvyl/sample/swf/checkout/workflow/api/CheckoutWorkflow.java
@@ -0,0 +1,20 @@
+package com.orvyl.sample.swf.checkout.workflow.api;
+
+import com.amazonaws.services.simpleworkflow.flow.annotations.Execute;
+import com.amazonaws.services.simpleworkflow.flow.annotations.Workflow;
+import com.amazonaws.services.simpleworkflow.flow.annotations.WorkflowRegistrationOptions;
+import com.orvyl.sample.swf.checkout.activities.api.domain.Order;
+
+/**
+ * Created by vyl on 10/2/15.
+ */
+@Workflow
+@WorkflowRegistrationOptions(
+        defaultExecutionStartToCloseTimeoutSeconds = 345600 // maximum of 4 days in every workflow execution
+)
+public interface CheckoutWorkflow {
+
+    @Execute(version = "1.0-SNAPSHOT")
+    void performCheckoutProcess(Order order);
+
+}
