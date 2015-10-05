449cce381f6ce7f6b41ea54f09bef8ac3b88ba8a
orvyl <OFTumaneng@smartemoneyinc.com>
2015-10-05 13:38:20 +0800

    added workflowApi in checkout

--- checkout-page-starter/src/main/java/com/orvyl/sampl/swf/checkout/starter/controller/HomeController.java
+++ checkout-page-starter/src/main/java/com/orvyl/sampl/swf/checkout/starter/controller/HomeController.java
@@ -1,14 +1,25 @@
 package com.orvyl.sampl.swf.checkout.starter.controller;
 
+import com.orvyl.sample.swf.checkout.activities.api.domain.Customer;
+import com.orvyl.sample.swf.checkout.activities.api.domain.Item;
+import com.orvyl.sample.swf.checkout.activities.api.domain.Order;
+import com.orvyl.sample.swf.checkout.workflow.api.CheckoutWorkflowAPI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 /**
  * Created by vyl on 10/5/15.
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    private CheckoutWorkflowAPI api;
 
     @RequestMapping("/")
     public String index() {
@@ -17,6 +28,36 @@ public class HomeController {
 
     @RequestMapping(value = "/checkout", method = RequestMethod.POST)
     public String checkout() {
+        String orderId = UUID.randomUUID().toString();
+
+        Order order = new Order(orderId);
+        order.setSubTotal(70.00);
+        order.setCustomer(new Customer("USER123", "Banung"));
+        ArrayList<Item> items = new ArrayList<>();
+
+        Item e = new Item();
+        e.setName("Tinapang maliliit");
+        e.setDescription("Small dried fish");
+        e.setPrice(50.00);
+        items.add(e);
+
+        Item e1 = new Item();
+        e1.setName("Bagoong");
+        e1.setDescription("Bagoong from Cagayan");
+        e1.setPrice(15.00);
+        items.add(e1);
+
+        Item e2 = new Item();
+        e2.setName("Kamatis");
+        e2.setDescription("Tomato");
+        e2.setPrice(5.00);
+        items.add(e2);
+
+        order.setItems(items);
+
+        // start of the workflow execution!
+        api.performCheckoutProcess(order);
+
         return "checkout";
     }
 
